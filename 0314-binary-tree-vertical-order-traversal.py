428b4d5cec2c8d038a10d4f42adb73ccd8e5377e
Rui Zhou <zhourui_1997@126.com>
2022-04-05 10:49:23 +0200

    :white_check_mark: :poop: Finish 314-二叉树的垂序遍历

--- 0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal.py
@@ -7,9 +7,33 @@ class Solution:
         if not root:
             return []
 
-        q = deque([])
+        q = deque()
+        q.append((root, 0, 0))
+        ans = deque()
+        ans.append([])
         leftMost, rightMost = 0, 0
+        while q:
+            currNode, currX, currY = q.popleft()
 
+            # needs to update ans
+            if currY < leftMost:
+                ans.appendleft([currNode.val])
+                leftMost = currY
+            elif currY > rightMost:
+                # 比如现在最右index, 即rightMost = 1, 来的currY = 2
+                ans.append([currNode.val])
+                rightMost = currY
+            else:
+                # 比如现在最左index, 即leftMost = -1, 来的currY = 1, 那就修改ans[2], 其中2 = currY - leftMost
+                ans[currY - leftMost].append(currNode.val)
+
+            if currNode.left:
+                q.append((currNode.left, currX + 1, currY - 1))
+
+            if currNode.right:
+                q.append((currNode.right, currX + 1, currY + 1))
+
+        return list(ans)
 
 
 if __name__ == '__main__':
