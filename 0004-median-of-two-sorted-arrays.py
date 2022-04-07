7339af809ddbda308b3ef8af1fe61b3c0f10b41e
Rui Zhou <zhourui_1997@126.com>
2022-04-07 19:21:29 +0200

    :sparkles: Init 4

---
+++ 0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        pass
+
+
+if __name__ == '__main__':
+    nums1 = [1, 3, 6, 8, 10, 15]
+    nums2 = [2, 4, 7, 11, 13, 14, 18, 20]
+    sol = Solution()
+    print(sol.findMedianSortedArrays(nums1, nums2))
