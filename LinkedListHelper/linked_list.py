2f7ac0ca0b9aefe95ecc193aa20448ca1f883295
Rui Zhou <zhourui_1997@126.com>
2022-06-02 16:16:34 +0200

    :sparkles: Import LinkedList Helper

---
+++ LinkedListHelper/linked_list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def arrayToListNode(numbers):
+    # Generate list from the array
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in numbers:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+
+    ptr = dummyRoot.next
+    return ptr
+
+
+def prettyPrintLinkedList(node):
+    while node and node.next:
+        print(str(node.val) + "->", end='')
+        node = node.next
+
+    if node:
+        print(node.val)
+    else:
+        print("Empty LinkedList")
