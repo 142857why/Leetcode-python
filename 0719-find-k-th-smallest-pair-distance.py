ef6f67a67d8940755b8d0f624f35cfd2804fc2ab
Rui Zhou <zhourui_1997@126.com>
2022-06-10 16:43:47 +0200

    :sparkles: Init 719-找出第k小的数对距离

---
+++ 0719-find-k-th-smallest-pair-distance.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    a, k = [1, 3, 1], 1
+    sol = Solution()
+    print(sol.smallestDistancePair(a, k))
