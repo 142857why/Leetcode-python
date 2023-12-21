20168d6274a4b516a5609cad3e17117198391fee
Rui Zhou <zhourui_1997@126.com>
2023-12-21 17:22:40 +0000

    :white_check_mark: Finish 208

--- 0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree.py
@@ -1,16 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+        self.word = None
+
+
 class Trie:
 
     def __init__(self):
-        pass
+        self.root = TrieNode()
+
+    def get_root(self):
+        return self.root
 
     def insert(self, word: str) -> None:
-        pass
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+
+            node = node.children[char]
+
+        node.is_word = True
+        node.word = word
 
     def search(self, word: str) -> bool:
-        pass
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+
+            node = node.children[char]
+
+        return node.is_word
 
     def startsWith(self, prefix: str) -> bool:
-        pass
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+
+            node = node.children[char]
+
+        return True
 
 
 # Your Trie object will be instantiated and called as such:
@@ -19,4 +51,10 @@ class Trie:
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
 if __name__ == '__main__':
-    pass
+    obj = Trie()
+    obj.insert("apple")
+    print(obj.search("apple"))
+    print(obj.search("app"))
+    print(obj.startsWith("app"))
+    obj.insert("app")
+    print(obj.search("app"))
