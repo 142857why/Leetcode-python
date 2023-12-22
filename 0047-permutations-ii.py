693e151c692a2fbdbe5fbbaa58960ddbca1b3bf7
Rui Zhou <zhourui_1997@126.com>
2023-12-22 16:39:36 +0000

    :white_check_mark: Finish 47

--- 0047-permutations-ii.py
+++ 0047-permutations-ii.py
@@ -3,7 +3,36 @@ from typing import List
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        pass
+        nums.sort()
+        visited = [False] * len(nums)
+        permutations = []
+        self.dfs(nums, visited, [], permutations)
+        return permutations
+
+    def dfs(self, nums, visited, permutation, permutations):
+        if len(nums) == len(permutation):
+            permutations.append(permutation)
+            return
+
+        for i in range(len(nums)):
+            if visited[i]:
+                continue
+
+            # This is the key to avoid duplicates
+            # 不同位置上的相同数字，必须按照顺序用
+            # If the current number is the same as the previous number,
+            # and the previous number is not visited
+            # then skip the current number
+            if i > 0 and nums[i] == nums[i - 1] and not visited[i - 1]:
+                continue
+
+            visited[i] = True
+            permutation.append(nums[i])
+
+            self.dfs(nums, visited, permutation.copy(), permutations)
+
+            permutation.pop()
+            visited[i] = False
 
 
 if __name__ == '__main__':
