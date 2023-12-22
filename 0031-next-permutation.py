68a95c484837f25dd7c3308451d31c931ffe1706
Rui Zhou <zhourui_1997@126.com>
2023-12-22 16:53:56 +0000

    :sparkles: Init 31-下一个排列

---
+++ 0031-next-permutation.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.nextPermutation([1, 2, 3]))
+    print(solution.nextPermutation([3, 2, 1]))
+    print(solution.nextPermutation([1, 1, 5]))
