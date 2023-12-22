3f05d82d28dcca0122f32f23c12dc52b1609ae2c
Rui Zhou <zhourui_1997@126.com>
2023-12-22 17:51:02 +0000

    :white_check_mark: Finish 39

--- 0039-combination-sum.py
+++ 0039-combination-sum.py
@@ -3,7 +3,22 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        pass
+        candidates.sort()
+        results = []
+        self.dfs(candidates, target, 0, [], results)
+        return results
+
+    def dfs(self, candidates: List[int], target: int, start: int, combination: List[int], results: List[List[int]]) -> None:
+        if target < 0:
+            return
+        if target == 0:
+            results.append(combination[:])
+            return
+
+        for i in range(start, len(candidates)):
+            combination.append(candidates[i])
+            self.dfs(candidates, target - candidates[i], i, combination, results)
+            combination.pop()
 
 
 if __name__ == '__main__':
