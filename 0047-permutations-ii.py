41e41eaee3507dcc7143b179a847a76228c125ea
Rui Zhou <zhourui_1997@126.com>
2023-12-22 16:02:53 +0000

    :sparkles: Init 47-全排列II

---
+++ 0047-permutations-ii.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.permuteUnique([1, 1, 2]))
+    print(solution.permuteUnique([1, 2, 3]))
