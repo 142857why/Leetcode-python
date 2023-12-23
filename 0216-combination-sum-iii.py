239b97cac891082d54ef1002aacce5c84269b896
Rui Zhou <zhourui_1997@126.com>
2023-12-23 13:04:17 +0000

    :white_check_mark: Finish 216

--- 0216-combination-sum-iii.py
+++ 0216-combination-sum-iii.py
@@ -3,7 +3,24 @@ from typing import List
 
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        pass
+        results = []
+        # k is the number of elements in the combination
+        # n is the sum of the combination
+        self.dfs(k, n, 1, [], results)
+        return results
+
+    def dfs(self, k: int, n: int, start: int, combination: List[int], results: List[List[int]]) -> None:
+        if k == 0 and n == 0:
+            results.append(combination[:])
+            return
+
+        for i in range(start, 10):
+            combination.append(i)
+            # k - 1 because we have added one element to the combination
+            # n - i because we have added i to the combination
+            # i + 1 because we cannot reuse the same element
+            self.dfs(k - 1, n - i, i + 1, combination, results)
+            combination.pop()
 
 
 if __name__ == '__main__':
