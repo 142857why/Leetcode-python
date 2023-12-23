4b7f5d90f327bbadc40711f393c9f470876ef827
Rui Zhou <zhourui_1997@126.com>
2023-12-23 19:33:14 +0000

    :sparkles: Init 90-子集2

---
+++ 0090-subsets-ii.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums = [1, 2, 2]
+    print(solution.subsetsWithDup(nums))
+    nums = [0]
+    print(solution.subsetsWithDup(nums))
