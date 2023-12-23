02249f00b3b88da3b7eb89c6646d5ee463f68dea
Rui Zhou <zhourui_1997@126.com>
2023-12-23 11:50:05 +0000

    :sparkles: Init 216-组合总和3

---
+++ 0216-combination-sum-iii.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    k = 3
+    n = 7
+    print(solution.combinationSum3(k, n))
+    k = 3
+    n = 9
+    print(solution.combinationSum3(k, n))
+    k = 4
+    n = 1
+    print(solution.combinationSum3(k, n))
+    k = 3
+    n = 2
+    print(solution.combinationSum3(k, n))
+    k = 9
+    n = 45
+    print(solution.combinationSum3(k, n))
