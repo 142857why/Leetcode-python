e41e7880b60cc5751fbc42191db8adc7a85c3294
Rui Zhou <zhourui_1997@126.com>
2023-12-23 20:23:51 +0000

    :white_check_mark: Finish 90

--- 0090-subsets-ii.py
+++ 0090-subsets-ii.py
@@ -3,7 +3,21 @@ from typing import List
 
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        pass
+        results = []
+        nums.sort()
+        self.dfs(nums, 0, [], results)
+        return results
+
+    def dfs(self, nums: List[int], startIndex: int, subset: List[int], results: List[List[int]]) -> None:
+        results.append(subset[:])
+
+        for i in range(startIndex, len(nums)):
+            if i > startIndex and nums[i] == nums[i - 1]:
+                continue
+
+            subset.append(nums[i])
+            self.dfs(nums, i + 1, subset, results)
+            subset.pop()
 
 
 if __name__ == '__main__':
