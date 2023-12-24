df4060e790f28e172175183e13ef4974e03a5c3f
Rui Zhou <zhourui_1997@126.com>
2023-12-24 13:41:47 +0000

    :white_check_mark: Finish 102

--- 0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal.py
@@ -1,3 +1,4 @@
+import collections
 from typing import List, Optional
 
 from TreeHelper.binary_tree import *
@@ -5,7 +6,23 @@ from TreeHelper.binary_tree import *
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        pass
+        if root is None:
+            return []
+
+        results = []
+        queue = collections.deque([root])
+        while queue:
+            levelSize = len(queue)
+            currentLevel = []
+            for _ in range(levelSize):
+                currentNode = queue.popleft()
+                currentLevel.append(currentNode.val)
+                if currentNode.left:
+                    queue.append(currentNode.left)
+                if currentNode.right:
+                    queue.append(currentNode.right)
+            results.append(currentLevel)
+        return results
 
 
 if __name__ == '__main__':
