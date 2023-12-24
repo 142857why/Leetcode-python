4f72e55d2470d8dbde6ac8175383dbc31612c2b0
Rui Zhou <zhourui_1997@126.com>
2023-12-24 14:08:20 +0000

    :white_check_mark: Finish 77
    朴素dfs

--- 0077-combinations.py
+++ 0077-combinations.py
@@ -3,7 +3,21 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        pass
+        results = []
+        self.dfs(n, k, 1, [], results)
+        return results
+
+    def dfs(self, n: int, k: int, start: int, combination: List[int], results: List[List[int]]):
+        if k == 0:
+            results.append(combination[:])
+            return
+
+        for i in range(start, n + 1):
+            combination.append(i)
+            # k - 1 because we have added one element to the combination
+            # i + 1 because we cannot reuse the same element
+            self.dfs(n, k - 1, i + 1, combination, results)
+            combination.pop()
 
 
 if __name__ == '__main__':
