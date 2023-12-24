d1e1a64a47f9c808baebf3af85484b74b6fca298
Rui Zhou <zhourui_1997@126.com>
2023-12-24 14:03:24 +0000

    :sparkles: Init 77-组合

---
+++ 0077-combinations.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    n = 4
+    k = 2
+    print(solution.combine(n, k))
+    n = 1
+    k = 1
+    print(solution.combine(n, k))
